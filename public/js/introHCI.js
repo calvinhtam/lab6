ca2114d956080fbcd7e3307f30a27f77e03ffb74
Calvin Tam <c6tam@ucsd.edu>
2020-02-13 02:25:24 -0800

    finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,5 +26,27 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	// Gets the project url
+	var url = "/project/" + idNumber;
 	console.log("User clicked on project " + idNumber);
+	console.log("Project URL: " + url);
+
+	$.get(url, callBackFn);
+}
+
+/*
+ * Make an AJAX call to retrieve project details and add it in
+ */
+function callBackFn(result) {
+	console.log(result);
+	var curr_id = result.id;
+	console.log(curr_id);
+
+	var projectHTML =
+		'<p>' + result['title'] + '</p>' +
+		'<p>' + result['date'] + '</p>' +
+		'<p><img src="' + result['image'] + '" class="detailsImage">' +
+		result['summary'] + '</p>';
+
+	$('#project' + curr_id).find('.details').html(projectHTML);
 }
